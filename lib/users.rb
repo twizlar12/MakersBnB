a32184a8505669a1cb1edb728f23d024cceb5960
Adewunmi Awolesi <adewunmi.awolesi@beis.gov.uk>
2020-05-26 16:28:13 +0100

    classes created

---
+++ lib/users.rb
@@ -0,0 +1,16 @@
+class Users
+
+  def initialize(name, password)
+    @name = name 
+    @password = password
+  end 
+
+  def user_to_database 
+    #pass new user to database
+  end 
+
+  def signin
+    #verify user is in the database
+  end
+  
+end 
