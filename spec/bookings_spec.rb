5a471dca0dd54dfbd9fb9928b189935ea7ac8344
Adewunmi Awolesi <adewunmi.awolesi@beis.gov.uk>
2020-05-28 14:23:16 +0100

    investigating date type error

--- spec/bookings_spec.rb
+++ spec/bookings_spec.rb
@@ -1,16 +1,16 @@
 require "./lib/bookings.rb"
+require 'date'
 
 describe Bookings do
   it 'can list dates a place is booked' do
-    booking = Bookings.new(property_name: "Holiday home", start_date: 2010-01-01, end_date: 2010-01-02)
+    booking = Bookings.new(property_name: "Holiday home", start_date: '2010-01-01', end_date: '2010-01-02')
     expect(booking).to be_instance_of Bookings
   end
   
   it 'can make a booking' do
-    booking = Bookings.add(property_name: "Holiday home", start_date: 2010-01-01, end_date: 2010-01-02)
+    booking = Bookings.add(property_name: "Holiday home", start_date: '2010-01-01', end_date: '2010-01-02')
     
-    expect(Bookings.all.first).to eq 2010-01-01
-    expect(Bookings.all.last).to eq 2010-01-02
+    expect(Bookings.all.first).to eq '2010-01-01'
+    expect(Bookings.all.last).to eq '2010-01-02'
   end
 end
-
