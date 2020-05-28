545b84325ef84c05b80176892d446647e607f442
Cassis <cassismjones@gmail.com>
2020-05-28 11:30:59 +0100

    Users tests now passing sign up and sign in

--- spec/database_helper.rb
+++ spec/database_helper.rb
@@ -6,15 +6,3 @@ def connect_and_add_spaces
   Spaces.add(bedrooms: 10, location: 'Hertfordshire')
   Spaces.add(bedrooms: 3, location: 'Canary Wharf')
 end
-
-def persisted_data_signup(name:)
-  connection = PG.connect(dbname: 'makers_bnb_test')
-  result = connection.query("SELECT name FROM users WHERE name LIKE '#{name}%';")
-  result.first
-end
-
-def persisted_data_signin(name:)
-  connection = PG.connect(dbname: 'makers_bnb_test')
-  result = connection.query("SELECT name FROM users WHERE name LIKE '#{name}%';")
-  result.first
-end
